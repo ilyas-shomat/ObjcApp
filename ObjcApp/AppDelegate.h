4fc29c20d3180465e79d97a714fb9fb99cb7059e
Ilyas Shomat <ilyas.shomat@gmail.com>
2022-12-10 20:25:13 +0600

    Initial Commit

---
+++ ObjcApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ObjcApp
+//
+//  Created by Ilyas Shomat on 10.12.2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ObjcApp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ObjcApp
+//
+//  Created by Ilyas Shomat on 10.12.2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
